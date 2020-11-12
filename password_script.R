2daa7b1c8958c47cdf42b856228484f7d6e58a39
Katie Weber <70818693+kweber142@users.noreply.github.com>
2020-11-12 14:48:30 -0500

    Update password_script.R

--- password_script.R
+++ password_script.R
@@ -5,22 +5,17 @@ library(readr)
 library(gt)
 
 # The data
-# This data table has passwords, strength, and a whole bunch of other data
-# It also includes 2 columns which would tell you how long it would take to guess a password by just testing random passwords
-# One of the columns has the "value" and one of the columns has the "time_unit", like days, hours, etc.
 passwords <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-14/passwords.csv')
 
 # Cleaning up the data
-
-# This creates a table that summarizes some data based on different categories of passwords
 passwords_category <- passwords %>%
   group_by(category) %>%
   summarize(avg_strength = mean(strength), 
-            avg_offline = mean(offline_crack_sec))%>%
+            avg_offline = mean(offline_crack_sec,
+            avg_guess_min = mean(value)))%>%
   ungroup(time_unit) %>%
   drop_na()
 
-# This table provides the top 3 most common passwords for each category from the data source
 password_examples <- passwords %>%
   select(rank, password, category) %>%
   group_by(category) %>%
@@ -29,29 +24,8 @@ password_examples <- passwords %>%
   mutate(rank = rep(c(1,2,3))) %>%
   pivot_wider(names_from = rank, values_from = password)
 
-# This table joins the passwords_category and password_examples tables so you can see everything at once
 passwords_full <- passwords_category %>%
   left_join(password_examples, by = "category")
 
-# I want to figure out how to find the average time to crack the password by randomly guessing
-# So I created this "fix_value" function, which would theoretically make all values and time_units the same (hours)
-# And then I would need to summarize this data for each category
-# I do not know how to apply this to the passwords_category table above so that I have all of the summarized data there
-
-fix_value <- function(value, time_unit) {
-  if (time_unit == "seconds") {
-    return(value/3600)
-  } else if (time_unit == "minutes") {
-    return(value/60)
-  } else if (time_unit == "days") {
-    return(value*24)
-  } else if (time_unit == "months") {
-    return(value*720)
-  } else if (time_unit == "years") {
-    return(value*8640)
-  } else {
-    return(value)
-  }
-}
   
   
